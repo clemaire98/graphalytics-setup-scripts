2b1b0bbc5f81f3b454630726fff7221c8447c891
Chris Lemaire <chris.lemaire@telfort.nl>
2017-05-14 20:44:24 +0200

    Add giraph build and install

--- src/giraph/install.sh
+++ src/giraph/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Declare openg install directory.
-INSTALL_DIR=powergraph-v2.2
+# Declare giraph install directory.
+INSTALL_DIR=giraph-1.2.0
 
 # Declare the name of the config directory of graphalytics
 CONF_DIR=config-template
 
-# Declare openg intermediate directory.
-#OPENG_INT=tmp/openg
+# Declare giraph intermediate directory.
+#giraph_INT=tmp/giraph
 
 # Get the path of this script.
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
@@ -21,10 +21,10 @@ then
 fi
 
 
-# Path variables for paths of confs and PG_HOME
-PG_HOME=$_INST_HOME/$INSTALL_DIR/powergraph
-GRAPH_PLAT_HOME=$_INST_HOME/$INSTALL_DIR/graphalytics-platforms-powergraph
-PG_CONF=$_INST_HOME/$INSTALL_DIR/graphalytics-platforms-powergraph/$CONF_DIR/powergraph.properties
+# Path variables for paths of confs and GIRAPH_HOME
+GIRAPH_HOME=$_INST_HOME/$INSTALL_DIR/giraph
+GRAPH_PLAT_HOME=$_INST_HOME/$INSTALL_DIR/graphalytics-platforms-giraph
+GIRAPH_CONF=$_INST_HOME/$INSTALL_DIR/graphalytics-platforms-giraph/$CONF_DIR/giraph.properties
 
 
 
@@ -34,12 +34,16 @@ then
 	echo Removing $GRAPH_PLAT_HOME
 	rm -rf $GRAPH_PLAT_HOME
 
-	echo Removing $PG_HOME
-	rm -rf $PG_HOME
+	echo Removing $GIRAPH_HOME
+	rm -rf $GIRAPH_HOME
 
 	# Clone the necessary repositories and dependencies
-	git clone https://github.com/atlarge-research/graphalytics-platforms-powergraph.git $GRAPH_PLAT_HOME
-	git clone https://github.com/graphlab-code/graphlab.git $PG_HOME
+	git clone https://github.com/atlarge-research/graphalytics-platforms-giraph.git $GRAPH_PLAT_HOME
+	
+    # Get Giraph and unpack it
+    wget http://apache.xl-mirror.nl/giraph/giraph-1.2.0/giraph-dist-1.2.0-bin.tar.gz -P $GIRAPH_HOME
+    tar -xvzf $GIRAPH_HOME/giraph-dist-1.2.0-bin.tar.gz
+    rm -f $GIRAPH_HOME/giraph-dist-1.2.0-bin.tar.gz
 
 	# Rename config-template folder to config
 	#mv $GRAPH_PLAT_HOME/config-template $GRAPH_PLAT_HOME/$CONF_DIR
@@ -48,8 +52,8 @@ fi
 
 
 # Edit the config file of the graphalytics platform driver.
-echo Configuring \'$PG_CONF\'.
-$SCRIPT_PATH/../util/configure.sh "powergraph.home" $PG_HOME $PG_CONF
+echo Configuring \'$GIRAPH_CONF\'.
+$SCRIPT_PATH/../util/configure.sh "giraph.home" $GIRAPH_HOME $GIRAPH_CONF
 
 
 
