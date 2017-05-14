20f7e66ccb9760ee9e279e85218998ed6e244753
Chris Lemaire <chris.lemaire@telfort.nl>
2017-05-14 10:29:41 +0200

    Fix multiple issues

--- install-main.sh
+++ install-main.sh
@@ -1,38 +1,15 @@
-# Export the unpack directory, which should be the first parameter.
-_INST_HOME=$HOME
-
-while [ $# -gt 1 ]
-do
-	case "$1" in
-		-p|--path)
-			shift
-			
-			if [ $# -lt 1 ]
-			then
-				echo Expected start path after -p|--path.
-				exit
-			else
-				_INST_HOME="$1"
-			fi
-			;;
-		-c|--clean)
-			_INST_CLEAN=1
-			export _INST_CLEAN
-			;;
-	esac
-	shift
-done
-
-export _INST_HOME
+# Run the install-arguments script.
+./install-arguments.sh parse "$@"
 
 # Clone the graphalytics core repo
-git clone https://github.com/ldbc/ldbc_graphalytics.git
+if [ "$_INST_CLEAN" = "1" ]
+then
+	git clone https://github.com/ldbc/ldbc_graphalytics.git $HOME/ldcs-graphalytics
+fi
 
 
 # Install all underlying repos with the same options
-./openg/install-openg.sh "$@"
+./openg/install-openg.sh
 
-
-# Clean up temporary variables
-unset _INST_CLEAN
-unset _INST_HOME
+# Run the install-arguments script for cleaning up.
+../install-arguments.sh clean "$@"
--- openg/install-openg.sh
+++ openg/install-openg.sh
@@ -1,25 +1,29 @@
+# Run the install-arguments script.
+../install-arguments.sh parse "$@"
+
 # Name of the config directory of graphalytics
 CONF_DIR=conf
 
 # Path variables for paths of confs and openg_home
-OPENG_HOME=$PWD/graphBIG/
-OPENG_CONF=$PWD/graphalytics-platforms-openg/$CONF_DIR/openg.properties
+OPENG_HOME=$_INST_HOME/graphBIG
+GRAPH_PLAT_HOME=$_INST_HOME/graphalytics-platforms-openg
+OPENG_CONF=$_INST_HOME/graphalytics-platforms-openg/$CONF_DIR/openg.properties
 
-if [ "$_INST_CLEAN" -eq "1" ]
+if [ "$_INST_CLEAN" = "1" ]
 then
 	# Delete the current packages to be sure of a clean install
-	echo Removing ./graphalytics-platforms-openg/
-	rm -rf ./graphalytics-platforms-openg/
+	echo Removing $GRAPH_PLAT_HOME
+	rm -rf $GRAPH_PLAT_HOME
 
-	echo Removing ./graphBIG/
-	rm -rf ./graphBIG/
+	echo Removing $OPENG_HOME
+	rm -rf $OPENG_HOME
 
 	# Clone the necessary repositories and dependencies
 	git clone https://github.com/atlarge-research/graphalytics-platforms-openg
 	git clone https://github.com/graphbig/graphBIG.git
 
 	# Rename config-template folder to config
-	mv ./graphalytics-platforms-openg/config-template ./graphalytics-platforms-openg/$CONF_DIR
+	mv $GRAPH_PLAT_HOME/config-template $GRAPH_PLAT_HOME/$CONF_DIR
 fi
 
 # Edit the config file of graphalytics to point to the
@@ -27,3 +31,5 @@ fi
 echo Configuring $OPENG_CONF to have \'openg.home = $OPENG_HOME\'
 sed -i "s+\(\s*openg\.home\s*=\s*\)+\1$OPENG_HOME+" $OPENG_CONF
 
+# Run the install-arguments script for cleaning up.
+../install-arguments.sh clean "$@"
